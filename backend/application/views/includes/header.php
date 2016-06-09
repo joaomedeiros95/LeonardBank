756317dfc7bdd0698de39c53b889c61750918333
João Eduardo Medeiros <joaoribeiromedeiros@gmail.com>
2016-06-09 16:08:12 -0300

    Adicionado arquivo custom.css

--- backend/application/views/includes/header.php
+++ backend/application/views/includes/header.php
@@ -5,5 +5,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="<?php echo base_url(); ?>assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/main.css" />
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/ie8.css" /><![endif]-->
 	</head>
--- backend/application/views/login.php
+++ backend/application/views/login.php
@@ -12,10 +12,23 @@
 			<section id="banner">
 				<header>
 					<h2><strong>Login</strong></h2>
-					<p>
-						<input type="text" id="nomeid" placeholder="Login" required="required" name="nome" />
-					</p>
 				</header>
+				<form action="<?php echo base_url(); ?>/index.php/usuario_controller/login" method="post">
+					<div class="form-login">
+						<p>
+							<input type="text" id="nomeid" placeholder="Usuário/Email" required="required" name="usuario" size="50" />
+						</p>
+						<p>
+							<input type="password" id="senha" placeholder="Senha" required="required" name="senha" size="50" />
+						</p>
+						<p>
+							<input type="submit" value="Submit">
+						</p>
+						<p>
+							Não possui login? <a href="#artista">Cadastre-se</a>.
+						</p>
+					</div>
+				</form>
 			</section>
 
 	</div>
